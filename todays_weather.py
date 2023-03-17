733ebf4a361499c67550212a365d42e789d4923b
Justin Liu <justinliu1308@gmail.com>
2023-03-17 13:08:07 -0400

    formatting into neat code

--- todays_weather.py
+++ todays_weather.py
@@ -2,28 +2,33 @@ import requests
 import json
 from beautifultable import BeautifulTable
 
-# Fetch weather data using your unique API key from https://openweathermap.org/
-API_KEY = open('api_key.txt', 'r').read()
-current_weather_url = 'http://api.openweathermap.org/data/2.5/weather'
-parameters = {
-    'q': 'Seattle',
-    'appid': API_KEY,
-    'units': 'imperial'
-}
-current_weather_response = requests.get(current_weather_url, params=parameters)
-# Receive and format response
-if current_weather_response.status_code == 200:
+
+def get_weather(city):
+    # Fetch weather data using your unique API key from https://openweathermap.org/
+    API_KEY = open('api_key.txt', 'r').read()
+    current_weather_url = 'http://api.openweathermap.org/data/2.5/weather'
+    parameters = {
+        'q': city,
+        'appid': API_KEY,
+        'units': 'imperial'
+    }
+    current_weather_response = requests.get(current_weather_url, params=parameters)
+    return current_weather_response
+
+def create_table(current_weather_response, city):
+    # Receive response and format JSON data
+
     current_weather_response = current_weather_response.json()
     # Write JSON data to file to view
     with open("json_weather.json", "w") as file:
         json.dump(current_weather_response, file, indent=4)
     # Assigning weather data variables
-    table = BeautifulTable()
     summary = current_weather_response['weather'][0]['description']
     real_temp = str(round(current_weather_response['main']['temp'])) + " °F"
     feels_like = str(round(current_weather_response['main']['feels_like'])) + " °F"
     wind = str(round(current_weather_response['wind']['speed'])) + " mph"
     cloud = str(current_weather_response['clouds']['all']) + "% cloudy"
+    # Check for rain or snow, included in JSON only if present in current weather conditions
     rain = 0
     if 'rain' in current_weather_response:
         rain = str(current_weather_response['rain']['1h']) + " in"
@@ -36,6 +41,7 @@ if current_weather_response.status_code == 200:
     else:
         timezone = "UTC" + str(timezone)
     # Creating table
+    table = BeautifulTable()
     table.rows.append([summary])
     table.rows.append([real_temp])
     table.rows.append([feels_like])
@@ -46,27 +52,23 @@ if current_weather_response.status_code == 200:
     if snow:
         table.rows.append([snow])
     table.rows.append([timezone])
-    table.columns.header = [parameters['q'] + " Weather"]
+    table.columns.header = [city + " Weather"]
     if rain and snow:
-        table.rows.header = ["Summary", "Real Temp", "Feels Like", "Wind", "Cloudiness", "Rainfall", "Snowfall", "Timezone"]
+        table.rows.header = ["Summary", "Real Temp", "Feels Like", "Wind", "Cloudiness", "Rain", "Snow", "Timezone"]
     elif rain:
-        table.rows.header = ["Summary", "Real Temp", "Feels Like", "Wind", "Cloudiness", "Rainfall", "Timezone"]
+        table.rows.header = ["Summary", "Real Temp", "Feels Like", "Wind", "Cloudiness", "Rain", "Timezone"]
     elif snow:
-        table.rows.header = ["Summary", "Real Temp", "Feels Like", "Wind", "Cloudiness", "Snowfall", "Timezone"]
+        table.rows.header = ["Summary", "Real Temp", "Feels Like", "Wind", "Cloudiness", "Snow", "Timezone"]
     else:
         table.rows.header = ["Summary", "Real Temp", "Feels Like", "Wind", "Cloudiness", "Timezone"]
     table.set_style(BeautifulTable.STYLE_BOX_ROUNDED)
     print(table)
+
+
+city = 'District of Columbia'
+response = get_weather(city)
+if response.status_code == 200:
+    create_table(response, city)
 else:
-    print(f"Error fetching weather data: status code {current_weather_response.status_code}")
+    print(f"Error fetching weather data: status code {response.status_code}")
 
-
-# forecast_url = 'http://api.openweathermap.org/data/2.5/forecast'
-# forecast_response = requests.get(forecast_url, params=parameters)
-# print(forecast_response.url)
-
-#     weather_data = response.json()
-#     temperature = weather_data['main']['temp']
-#     humidity = weather_data['main']['humidity']
-#     description = weather_data['weather'][0]['description']
-#     return f"The temperature in {city} is {temperature}°C, the humidity is {humidity}%, and the weather is {description}."
